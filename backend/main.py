4dd64acca1b49642aeb4299b903aa1ba3fc4c43c
dunura13 <dunurae@gmail.com>
2025-11-23 03:58:04 -0500

    implemented gemini api in slack bot responses

--- backend/main.py
+++ backend/main.py
@@ -1,53 +1,123 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
-from openai import OpenAI
+from fastapi import FastAPI, Request, BackgroundTasks
+import google.generativeai as genai
 from supabase import create_client
-from slack_bolt import App
-from slack_bolt.adapter.fastapi import SlackRequestHandler
+from slack_sdk import WebClient
+import json
 
 
 
 load_dotenv() # load environemnt variables
 
-slack_app = App( # initalize slack app
-    token = os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-)
+slack_client = WebClient( # initalize slack app
+    token = os.environ.get("SLACK_BOT_TOKEN"))
 
-handler = SlackRequestHandler(slack_app)
+# configure gemini
+genai.configure(api_key = os.environ.get("GEMINI_KEY"))
+model = genai.GenerativeModel("gemini-2.5-flash")
 
-try:
-    #test openAI connection
-    ai_client = OpenAI(api_key=os.environ.get("OPENAI_KEY"))
 
-    # test supabase connection
-
-    supabase = create_client(
-        os.environ.get("SUPABASE_URL"),
-        os.environ.get("SUPABASE_KEY")
-        )
-
-    print("Keys are working")
-
-except Exception as e:
-    print(f"Error loading keys: {e}")
-
-# create FastAPI app
 
 app = FastAPI()
 
+
+# runs after server replies to slack
+def process_decision(event: dict):
+    channel_id = event.get("channel") # channel which event occurd
+    user_id = event.get("user") # user who mentioned the bot
+    text = event.get("text") # text of the message
+    thread_ts = event.get("thread_ts", event.get("ts")) # timestamp, if thread_ts is not present, use ts
+    
+    print(f"AI processing: {text}..")
+
+
+    try:
+        prompt = f"""
+        You are a project manager bot. Analyze this message: "{text}"
+        
+        If a technical decision is made, extract it into this JSON format:
+        {{ "decision": "...", "rationale": "...", "action_items": "..." }}
+        
+        If NO decision is made, return exactly: {{ "decision": false }}
+        """
+
+        # CALL gemini api
+        response = model.generate_content(
+            prompt,
+            generation_config={"response_mime_type": "application/json"}
+        )
+    
+
+        
+        # parse result
+        result_json = response.text
+        result_data = json.loads(result_json)
+
+
+
+        # Construct a reply
+        if result_data.get("decision"):
+            reply_text = (
+                f"**Decision Detected**\n"
+                f"> *{result_data['decision']}*\n"
+                f"**Rationale:**{result_data['rationale']}"
+            )
+        
+        else:
+            reply_text = "Analyzed this but didnt find a firm decision. Try being more specific!"
+
+        
+        # send reply to slack
+        slack_client.chat_postMessage(
+            channel=channel_id,
+            text = reply_text,
+            thread_ts = thread_ts
+        )
+
+        print("Reply sent to slack")
+
+    except Exception as e:
+        print(f"GEMINI/SLACK error: {e}")
+
+
+
 @app.post("/slack/events")
-async def slack_events(req: Request):
-    return await handler.handle(req)
+async def slack_events(request: Request, background_tasks: BackgroundTasks):
+    # grab headers (check for retries)
+    headers = request.headers
+    retry_num = headers.get("x-slack-retry-num")
+
+    # parse data
+    body = await request.json()
+
+    # handle URL verification
+    if body.get("type") == "url_verification":
+        return {"challenge":body["challenge"]}
+    
+    # anti-duplicate logic 
+    if retry_num and int(retry_num) > 0:
+        return {"status": "ok", "message":"Ignored retry"}
+
+    # handle "App mention"
+    event = body.get("event", {})
+    if event.get("type") == "app_mention":
+        background_tasks.add_task(process_decision, event)
+
+    
+    return {"status": "ok"} # so slack does not timeout
+    
+
+
+
+
+
+
 
 @app.get("/")
 def health_check():
     return {"status":"alive", "message":"ThreadLock is running"}
 
-@slack_app.event("app_mention")
-def handle_app_mention(body, say):
-    say("Threadlock, at your service!")
 
 
 
